3046988bf91a40bf85b09248406aec4f8e0579d4
Eric Schaefer <omg@eric-schaefer.com>
2016-07-06 10:53:33 -0700

    webpack config: remove hot middleware for now, and add React as global

--- dist/server/webpack.config.js
+++ dist/server/webpack.config.js
@@ -1,7 +1,7 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+Object.defineProperty(exports, '__esModule', {
+	value: true
 });
 
 var _path = require('path');
@@ -17,26 +17,31 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var managerEntry = process.env.DEV_BUILD ? _path2.default.resolve(__dirname, '../../src/client/manager') : _path2.default.resolve(__dirname, '../manager');
 
 var config = {
-  devtool: '#cheap-module-eval-source-map',
-  entry: {
-    manager: [managerEntry],
-    preview: [_path2.default.resolve(__dirname, './error_enhancements'), 'webpack-hot-middleware/client']
-  },
-  output: {
-    path: _path2.default.join(__dirname, 'dist'),
-    filename: '[name].bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [new _webpack2.default.optimize.OccurenceOrderPlugin(), new _webpack2.default.HotModuleReplacementPlugin()],
-  module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      loader: 'babel',
-      query: { presets: ['react', 'es2015', 'stage-0'] },
-      exclude: [_path2.default.resolve('./node_modules'), _path2.default.resolve(__dirname, 'node_modules')],
-      include: [_path2.default.resolve('./'), __dirname, _path2.default.resolve(__dirname, '../../src')]
-    }]
-  }
+	devtool: '#cheap-module-eval-source-map',
+	entry: {
+		manager: [managerEntry],
+		preview: [_path2.default.resolve(__dirname, './error_enhancements')]
+	},
+	output: {
+		path: _path2.default.join(__dirname, 'dist'),
+		filename: '[name].bundle.js',
+		publicPath: '/static/'
+	},
+	plugins: [
+		new _webpack2.default.optimize.OccurenceOrderPlugin(),
+		new _webpack.ProvidePlugin({
+			'React': 'react',
+		}),
+	],
+	module: {
+		loaders: [{
+			test: /\.jsx?$/,
+			loader: 'babel',
+			query: { presets: ['react', 'es2015', 'stage-0'] },
+			exclude: [_path2.default.resolve('./node_modules'), _path2.default.resolve(__dirname, 'node_modules')],
+			include: [_path2.default.resolve('./'), __dirname, _path2.default.resolve(__dirname, '../../src')]
+		}]
+	}
 };
 
 exports.default = config;
